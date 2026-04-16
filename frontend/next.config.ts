bd96437a40c47babb3715343c8ac0f090f95b88a
ShahwarT94 <shahwarshamimakhtar@gmail.com>
2026-04-16 14:07:01 -0700

    remove static export for dynamic routes

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",          // static export for S3 + CloudFront
+  
   trailingSlash: true,
   images: { unoptimized: true },
 };
